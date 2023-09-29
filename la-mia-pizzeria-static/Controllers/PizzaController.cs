101f261fe37575f60a8b5f8fb91623d5512810a1
Brody14 <stefania.spinas@gmail.com>
2023-09-29 14:29:24 +0200

    FEAT database

--- la-mia-pizzeria-static/Controllers/PizzaController.cs
+++ la-mia-pizzeria-static/Controllers/PizzaController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using la_mia_pizzeria_static.DataBase;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace la_mia_pizzeria_static.Controllers
 {
@@ -6,7 +8,12 @@ namespace la_mia_pizzeria_static.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            using (PizzaContext db = new PizzaContext())
+            {
+                List<Pizza> pizzas = db.Pizzas.ToList<Pizza>();
+
+                return View("Index", pizzas);
+            }
         }
     }
 }
---
+++ la-mia-pizzeria-static/Models/Pizza.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Column(TypeName = "text")]
+        public string Description { get; set; }
+
+        [MaxLength(500)]
+        public string Image {  get; set; }
+
+        [Column(TypeName = "decimal(5, 2)")]
+        public double Price { get; set; }
+
+        [Range(0, 5)]
+        public int Rating { get; set; }
+
+        public Pizza(string name, string description, string image, double price, int rating)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.Image = image;
+            this.Price = price;
+            this.Rating = rating;
+        }
+    }
+}

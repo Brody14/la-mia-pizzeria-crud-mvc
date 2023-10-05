9739c6e94c4fd0a83692f59731b4b689c6469e35
Brody14 <stefania.spinas@gmail.com>
2023-10-05 12:27:42 +0200

    EDIT migration

--- la-mia-pizzeria-static/DataBase/PizzaContext.cs
+++ la-mia-pizzeria-static/DataBase/PizzaContext.cs
@@ -8,6 +8,8 @@ namespace la_mia_pizzeria_static.DataBase
         public DbSet<Pizza> Pizzas { get; set; }
         public DbSet<Category> Categories { get; set; }
 
+        public DbSet<Ingredient> Ingredients { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=GrobariPizza;Integrated Security=True;TrustServerCertificate=True");

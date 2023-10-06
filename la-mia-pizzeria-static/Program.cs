4ef12353e764c443237ae6773bb82073739b2fbd
Brody14 <stefania.spinas@gmail.com>
2023-10-06 12:13:19 +0200

    FEAT login partial

--- la-mia-pizzeria-static/Program.cs
+++ la-mia-pizzeria-static/Program.cs
@@ -1,7 +1,14 @@
 using la_mia_pizzeria_static.CustomLoggers;
 using la_mia_pizzeria_static.DataBase;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<PizzaContext>();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<PizzaContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -24,10 +31,14 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Pizza}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
